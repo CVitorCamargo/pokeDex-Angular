f6ccac27e89d5b24920d41cb60321d20e94462e3
CVitorCamargo <vitor.camargo@msn.com>
2022-11-14 21:36:58 -0300

    Pokemon Search system

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  pokemon:PokemonData = {
+    id:0,
+    name:'',
+    sprites:{
+      front_default:''
+    },
+    types:[]
+  }
+
+  constructor(
+    private service: PokemonService
+  ) { }
+
+  ngOnInit(): void {
+    this.getPokemon('pikachu')
+  }
+
+  getPokemon(pokemon:string){
+    this.service.getPokemon(pokemon).subscribe(
+      {
+      next:(res)=>{
+        this.pokemon = {
+          id: res.id,
+          name: res.name,
+          sprites: res.sprites,
+          types: res.types
+        }
+        console.log(this.pokemon)
+      },
+      error:(err)=> console.log(err)
+      }      
+    )
+  }
+}
